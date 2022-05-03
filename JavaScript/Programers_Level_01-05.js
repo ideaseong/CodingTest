dfb32578aa4ca380d6faa27d12e5315fb5d9f797
ideaseong <ideaseong@gmail.com>
2022-05-03 19:27:16 +0900

    [프로그래머스] 키패드 누르기

---
+++ JavaScript/Programers_Level_01-05.js
@@ -0,0 +1,44 @@
+function solution(numbers, hand) {
+    const answer = '';
+    const left = 10;
+    const right = 12;
+    
+    for (number of numbers) {
+        if (number === 0) {
+            number = 11;
+        }
+        
+        if (number % 3 === 1) {
+            answer += "L";
+            left = number;
+        }
+        else if (number % 3 === 2) {
+            answer += "R";
+            right = number;
+        }
+        else {
+            const difL = Math.abs(number - left) ;
+            const difR = Math.abs(right - number - 2) ;
+            if (difL == difR) {
+                if (hand === "left"){
+                    answer += "L"; 
+                    left = number;
+                }
+                else{
+                    answer += "R"; 
+                    right = number;
+                }
+            }
+            else if (difL > difR) {
+                answer += "R"; 
+                right = number;
+            }
+            else {
+                answer += "L"; 
+                left = number;
+            }
+        }
+
+    }
+    return answer;
+}
